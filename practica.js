6bf109f2ae5c4001f428ef11ed09d57a8f012e13
Perla Vargas <mireinapipona@gmail.com>
2023-06-01 17:44:17 -0300

    probando

---
+++ practica.js
@@ -0,0 +1,5 @@
+const jugador1 = {nombre: "1", edad: 44, posicion: "delantero"};
+const jugador2 = {nombre: "12", edad: 45, posicion: "delantero"};
+const jugador3 = {nombre: "3", edad: 44, posicion: "defensor"};
+const jugador4 = {nombre: "4", edad: 44, posicion: "defensor"};
+const jugador5 = {nombre: "5", edad: 44, posicion: "arquero"}
